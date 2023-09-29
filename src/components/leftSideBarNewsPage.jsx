6f6525223e64498e2e3f3f9e13e4d632765a7238
elinoza <hillcakmak@gmail.com>
2023-09-29 14:56:24 +0300

    Update leftSideBarNewsPage.jsx

--- src/components/leftSideBarNewsPage.jsx
+++ src/components/leftSideBarNewsPage.jsx
@@ -43,7 +43,7 @@ class LeftSideBarNewsPage extends React.Component {
 						<Image
 							className="coverPhoto"
 							fluid
-							src="https://www.zipjob.com/blog/wp-content/uploads/2020/08/linkedin-default-background-cover-photo-1.png"
+							src="https://images.unsplash.com/photo-1577017040065-650ee4d43339?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
 						/>
 					</Row>
 					<Row className="profilePicRow">
